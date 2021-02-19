fb58c78a6921c98d50e7bb828328e88368d484be
Sergio Martin Rubio <econsergio@gmail.com>
2021-02-19 10:42:11 +0100

    Uncomment

--- src/test/java/com/sergiomartinrubio/codewars/two/IntegerSquareRootTest.java
+++ src/test/java/com/sergiomartinrubio/codewars/two/IntegerSquareRootTest.java
@@ -8,13 +8,13 @@ public class IntegerSquareRootTest {
     @Test
     public void basicTests() {
 
-//        assertEquals("1", IntegerSquareRoot.integerSquareRoot("1"));
-//        assertEquals("2", IntegerSquareRoot.integerSquareRoot("5"));
-//        assertEquals("4", IntegerSquareRoot.integerSquareRoot("17"));
-//        assertEquals("10", IntegerSquareRoot.integerSquareRoot("100"));
-//        assertEquals("31", IntegerSquareRoot.integerSquareRoot("1000"));
-//        assertEquals("27", IntegerSquareRoot.integerSquareRoot("780"));
-//        assertEquals("152421548093487868711992623730429930751178496967", IntegerSquareRoot.integerSquareRoot("23232328323215435345345345343458098856756556809400840980980980980809092343243243243243098799634"));
+        assertEquals("1", IntegerSquareRoot.integerSquareRoot("1"));
+        assertEquals("2", IntegerSquareRoot.integerSquareRoot("5"));
+        assertEquals("4", IntegerSquareRoot.integerSquareRoot("17"));
+        assertEquals("10", IntegerSquareRoot.integerSquareRoot("100"));
+        assertEquals("31", IntegerSquareRoot.integerSquareRoot("1000"));
+        assertEquals("27", IntegerSquareRoot.integerSquareRoot("780"));
+        assertEquals("152421548093487868711992623730429930751178496967", IntegerSquareRoot.integerSquareRoot("23232328323215435345345345343458098856756556809400840980980980980809092343243243243243098799634"));
         assertEquals("3510457208086937291253621317073222057793129", IntegerSquareRoot.integerSquareRoot("12323309809809534545458098709854808654685688665486860956865654654654324238000980980980"));
 
     }
